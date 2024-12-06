519fc57b6c118923c828169f64c5eef58fdf6218
drrelyea <drrelyea@gmail.com>
2024-12-05 22:28:37 -0800

    cleaned it up

--- day6.py
+++ day6.py
@@ -5,19 +5,6 @@ from utils import load_advent_of_code
 
 data = load_advent_of_code(20245)
 print(data)
-# %%
-# data = [
-#     "....#.....",
-#     ".........#",
-#     "..........",
-#     "..#.......",
-#     ".......#..",
-#     "..........",
-#     ".#..^.....",
-#     "........#.",
-#     "#.........",
-#     "......#...",
-# ]
 for iline, line in enumerate(data):
     if "^" in line:
         starty = iline
@@ -26,104 +13,54 @@ for iline, line in enumerate(data):
 
 
 # %%
-currx = startx
-curry = starty
-currv = "up"
-positions = dict()
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 mapsize = len(data)
-while 0 <= currx < mapsize and 0 <= curry < mapsize:
-    # print(positions)
-    if (currx, curry) in positions:
-        if currv in positions[(currx, curry)]:
-            break
-        else:
-            positions[(currx, curry)].append(currv)
-    else:
-        positions[(currx, curry)] = [currv]
 
-    if currv == "up":
-        if curry == 0:
-            break
-        if data[curry - 1][currx] == "#":
-            currv = "right"
+
+def go_through_maze(data):
+    loop = False
+    currx = startx
+    curry = starty
+    positions = dict()
+    dirpointer = 0
+    while 0 <= currx < mapsize and 0 <= curry < mapsize:
+        currv = directions[dirpointer]
+        # print(positions)
+        if (currx, curry) in positions:
+            if currv in positions[(currx, curry)]:
+                loop = True
+                break
+            else:
+                positions[(currx, curry)].append(currv)
         else:
-            curry -= 1
-    elif currv == "down":
-        if curry == mapsize - 1:
-            break
-        if data[curry + 1][currx] == "#":
-            currv = "left"
+            positions[(currx, curry)] = [currv]
+
+        newy = curry + currv[0]
+        newx = currx + currv[1]
+        if 0 <= newy < mapsize and 0 <= newx < mapsize and datacopy[newy][newx] == "#":
+            dirpointer = (dirpointer + 1) % 4
         else:
-            curry += 1
-    elif currv == "right":
-        if currx == mapsize - 1:
-            break
-        if data[curry][currx + 1] == "#":
-            currv = "down"
-        else:
-            currx += 1
-    elif currv == "left":
-        if currx == 0:
-            break
-        if data[curry][currx - 1] == "#":
-            currv = "up"
-        else:
-            currx -= 1
+            curry = newy
+            currx = newx
+    return positions, loop
+
+
+positions, loop = go_through_maze(data)
 
 print(len(positions))
 # %%
 loopsum = 0
 mapsize = len(data)
-for ii in range(mapsize):
-    for jj in range(mapsize):
-        if data[ii][jj] != "#":
-            currx = startx
-            curry = starty
-            currv = "up"
-            positions = dict()
-            datacopy = data.copy()
-            datacopy[ii] = datacopy[ii][0:jj] + "#" + datacopy[ii][jj + 1 :]
-            loop = False
-            while 0 <= currx < mapsize and 0 <= curry < mapsize:
-                # print(positions)
-                if (currx, curry) in positions:
-                    if currv in positions[(currx, curry)]:
-                        loop = True
-                        break
-                    else:
-                        positions[(currx, curry)].append(currv)
-                else:
-                    positions[(currx, curry)] = [currv]
+oldtrack = positions
+del oldtrack[(startx, starty)]
+for jj, ii in oldtrack:
+    if data[ii][jj] != "#":
+        datacopy = data.copy()
+        datacopy[ii] = datacopy[ii][0:jj] + "#" + datacopy[ii][jj + 1 :]
+        positions, loop = go_through_maze(datacopy)
 
-                if currv == "up":
-                    if curry == 0:
-                        break
-                    if datacopy[curry - 1][currx] == "#":
-                        currv = "right"
-                    else:
-                        curry -= 1
-                elif currv == "down":
-                    if curry == mapsize - 1:
-                        break
-                    if datacopy[curry + 1][currx] == "#":
-                        currv = "left"
-                    else:
-                        curry += 1
-                elif currv == "right":
-                    if currx == mapsize - 1:
-                        break
-                    if datacopy[curry][currx + 1] == "#":
-                        currv = "down"
-                    else:
-                        currx += 1
-                elif currv == "left":
-                    if currx == 0:
-                        break
-                    if datacopy[curry][currx - 1] == "#":
-                        currv = "up"
-                    else:
-                        currx -= 1
-            if loop:
-                loopsum += 1
+        if loop:
+            loopsum += 1
 
+print(loopsum)
 # %%
